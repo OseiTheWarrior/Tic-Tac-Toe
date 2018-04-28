84c9f72317e10eeae4e0e01df39aa391d4c31d5a
OseiTheWarrior <38815572+OseiTheWarrior@users.noreply.github.com>
2018-04-28 20:48:20 +0000

    Update README.js

--- README.js
+++ README.js
@@ -15,6 +15,12 @@ Code Attempt 1
   
   <h1>TIC-TAC-TOE BOARD</h1>
   
+print(' | |');
+print(' ' + b[7] + ' |' + b[8] + ' |' + b[9]+ ' ');
+print('-------------');
+print(b[4]+' |' +b[5]+ ' |' +b[6]);
+print('-------------');
+print(b[1]+' |' +b[2]+ ' |' +b[3]);
     
   
   
